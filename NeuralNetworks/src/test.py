949ea235e01e49452166125c4e325f16a429de83
ckbr0 <crt@keber.xyz>
2021-03-28 21:27:55 +0200

    Bla2

--- NeuralNetworks/src/test.py
+++ NeuralNetworks/src/test.py
@@ -30,9 +30,9 @@ for i in range(10):
 r = network.evaluate(test_data)
 print(r)
 
-network = Network([784, 32, 16, 10], sigmoid, sigmoid_prime, cost_function_prime)
-for i in range(10):
-    network.stochastic_gradient_descent(training_data, 10, 3.0)
+network = Network([784, 32, 10], sigmoid, sigmoid_prime, cost_function_prime)
+for i in range(100):
+    network.stochastic_gradient_descent(training_data, 100, 6.0)
 r = network.evaluate(test_data)
 print(r)
 
